3dd4e8ed04f6dc61d10dae2358dc6556be31d464
Nikolay Shtipkov <nikolayshtipkov@gmail.com>
2023-11-27 10:35:01 +0200

    add get all endpoint in minimal api using coupon store

--- MinimalAPI/MagicVilla_CouponAPI/Program.cs
+++ MinimalAPI/MagicVilla_CouponAPI/Program.cs
@@ -1,3 +1,5 @@
+using MagicVilla_CouponAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -13,6 +15,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.MapGet("/api/couopon", () => 
+{
+    return Results.Ok(CouponStore.couponList);
+});
 
 app.UseHttpsRedirection();
 
